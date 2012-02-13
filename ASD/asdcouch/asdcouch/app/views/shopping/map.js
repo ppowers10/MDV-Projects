dcaab54dc1ecd550715d97b215b54e811c873bc5
Patrick Powers <ppowers@fullsail.edu>
2012-02-13 01:05:19 -0500

    first commit for couch db week 3

---
+++ ASD/asdcouch/asdcouch/app/views/shopping/map.js
@@ -0,0 +1,14 @@
+function(doc) {
+	if(doc.groups1[1] === "Shopping"){
+		emit(doc.groups1[1], {
+			"groups1"	: doc.groups1[1],
+			"name"		: doc.name[1],
+			"rating"	: doc.rating[1],
+			"comments"	: doc.comments[1],
+			"phonenum"	: doc.phonenum[1],
+			"email"		: doc.email[1],
+			"url"		: doc.url[1],
+			"location"	: doc.location[1]
+		});
+	}
+};
