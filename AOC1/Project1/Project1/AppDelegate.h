b2af31ca9f9b00523a50e6eeed87b6e95cb49dc2
Patrick Powers <ppowers@fullsail.edu>
2012-05-03 22:38:42 -0400

    Commit to master brank project1

---
+++ AOC1/Project1/Project1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Project1
+//
+//  Created by Patrick Powers on 5/3/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
