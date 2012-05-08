08ef5ef36d4f638a704a706984ad970c792a45bb
Patrick Powers <ppowers@fullsail.edu>
2012-05-07 22:43:34 -0400

    Commit all AOC1

---
+++ AOC1/Project2.2/Project2.2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project2.2
+//
+//  Created by Patrick Powers on 5/7/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
