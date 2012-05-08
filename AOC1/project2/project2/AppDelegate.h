4468198580f3f99cbf0984fff619fcbbd4f56589
Patrick Powers <ppowers@fullsail.edu>
2012-05-07 22:38:46 -0400

    Project 2 top part done

---
+++ AOC1/project2/project2/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Project2
+//
+//  Created by Patrick Powers on 5/7/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
