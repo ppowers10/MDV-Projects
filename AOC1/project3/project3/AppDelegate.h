2dbf4cdd39dbe7e7fed4516d589c131ef9b6d4e9
Patrick Powers <ppowers@fullsail.edu>
2012-05-14 21:29:39 -0400

    begin project3

---
+++ AOC1/project3/project3/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  project3
+//
+//  Created by Patrick Powers on 5/14/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AOC1/project3/project3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  project3
+//
+//  Created by Patrick Powers on 5/14/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
