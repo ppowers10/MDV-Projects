e69441f8a8294dc260dbe1221aacdb24e073695a
Patrick Powers <ppowers@fullsail.edu>
2012-05-21 23:07:01 -0400

    project 4 beginning

---
+++ AOC1/Project4/Project4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project4
+//
+//  Created by Patrick Powers on 5/21/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
