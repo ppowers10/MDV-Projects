62bdd626340f8fe992cd112a79705cbeb97d04d3
Patrick Powers <ppowers@fullsail.edu>
2012-06-27 23:27:32 -0400

    Accessor and mutator methods in the base class

---
+++ AOC2/week1/week1/DogFactory.h
@@ -0,0 +1,13 @@
+//
+//  DogFactory.h
+//  week1
+//
+//  Created by Patrick Powers on 6/27/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DogFactory : NSObject
+
+@end
---
+++ AOC2/week1/week1/DogFactory.m
@@ -0,0 +1,13 @@
+//
+//  DogFactory.m
+//  week1
+//
+//  Created by Patrick Powers on 6/27/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import "DogFactory.h"
+
+@implementation DogFactory
+
+@end
